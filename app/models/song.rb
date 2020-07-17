7806ac6fdb0d81734df7e8065febaba313924d6e
codewithirene567 <irenejinscott@gmail.com>
2020-07-17 16:28:34 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,25 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+
+  def artist_name=(artist)
+    self.artist = Artist.find_or_create_by(name: artist)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+ #we are adding this here to represent what song_notes are
+  def song_notes=(notes)
+    notes.each do |note|
+      self.notes.build(content: note)
+    end
+  end
+  #we are adding this to turn the genre id id number into an integer
+  def genre=(id)
+    self.genre_id = id.to_i
+  end
+
 end
